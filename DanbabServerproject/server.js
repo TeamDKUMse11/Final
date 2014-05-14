6d9f5bbba4bdff49ffd21fe9dbb2d13bdc6d6361
YounSeok <aster9725@gmail.com>
2014-05-14 14:51:22 +0900

    한글 깨짐 해결

--- DanbabServerproject/server.js
+++ DanbabServerproject/server.js
@@ -10,12 +10,33 @@ http.createServer(function (req, res) {
     var query = url.parse(req.url).query;
     var filename = querystr.parse(query).file + ".json";
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset = utf-8' });
 
     setTimeout(function() {
         fs.readFile("Data/"+ filename,'utf8',function(err,data) {
             if(err) {
-                res.write("File Read Error Check querystring");
+                switch(date.getDay()){
+                    case 1:
+                        filename="Mon.json";
+                        break;
+                    case 2:
+                        filename="Tue.json";
+                        break;
+                    case 3:
+                        filename="Wed.json";
+                        break;
+                    case 4:
+                        filename="Thu.json";
+                        break;
+                    case 5:
+                        filename="Fri.json";
+                        break;
+                    case 6:
+                        filename="Sat.json";
+                        break;
+                }
+                data = fs.readFileSync('Data/'+filename,'utf8');
+                res.write(data);
                 res.end();
             }
             else {
@@ -29,4 +50,3 @@ http.createServer(function (req, res) {
 
 }).listen(port);
 
-date.get
