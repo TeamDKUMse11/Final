0f975911bd9178533c32ef2c48170464886a23d4
심윤석(Youn Seok Shim) <aster9725@gmail.com>
2014-06-07 21:53:21 +0900

    식단 데이터 뽑아오기
    파싱 함수 만들어서 json파일 만들기
    server에서 일주일마다 긁어오게 만들기

---
+++ DanbabServerproject/crawl.js
@@ -0,0 +1,18 @@
+﻿var cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
+
+var url = [ 'http://www.dankook.ac.kr/web/kor/d1_3_1_1', //교직원식당
+            'http://www.dankook.ac.kr/web/kor/d1_3_1_2', //학생식당
+            'http://www.dankook.ac.kr/web/kor/d1_3_1_3', //기숙사식당
+            'http://www.dankook.ac.kr/web/kor/1196'];    //키친트리
+
+request(url[0], function (drror, response, html) {
+    if (error) { throw error };
+
+    var $ = cheerio.load(html);
+
+    $('#boardsec').each(function () {
+        fs.writeFile("교직원식당 메뉴.txt", $(this).text());
+    })
+});
